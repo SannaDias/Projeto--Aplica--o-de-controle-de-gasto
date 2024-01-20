fcd3839d7baecf73a9e72d20e1e94f31fcaf1964
SannaDias <sannapdias@gmail.com>
2024-01-20 13:22:36 -0300

    Validação do formulário de Registro com Javascript

--- Pages/register/register.js
+++ Pages/register/register.js
@@ -1,12 +1,69 @@
-function oncChangeEnail(){
+function onChangeEmail() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+}
 
+function onChangePassword() {
+    const password = form.password().value;
+    console.log("Password:", password);
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+    form.passwordMinLengthError().style.display = password.length >= 6 ? "none" : "block";
+    validatePasswordsMatch();
+    toggleRegisterButtonsDisabled();
+}
+
+function onChangeConfirmPassword(){
+  validatePasswordsMatch();
+  toggleRegisterButtonsDisabled()
+}
+
+function validatePasswordsMatch() {
+    const password =form.password().value;
+    const confirmPassword = form.confirmPassword().value;
+
+
+     form.confirmPasswordDosentMatchError().style.display = password == confirmPassword ? "none" : "block"; 
+}
+
+function toggleRegisterButtonsDisabled(){
+    form.registerButton().disabled = !isFormValid();
 
 }
 
+function isFormValid() {
+    const email = form.email().value;
+    if (!email || !validateEmail(email)) {
+        return false;
+    }
+
+    const password = form.password().value;
+    if (!password || password.length < 6) {
+        return false;
+    }
+
+    const confirmPassword = form.confirmPassword().value;
+    if (password !== confirmPassword) {
+        return false;
+    }
+
+    return true;
+}
+
+
+
 const form = {
-    confirmPassword:() => document.getElementById("confimPassword"),
+    confirmPassword: () => document.getElementById("confirmPassword"),
+    confirmPasswordDosentMatchError: () => document.getElementById("password-doesnt-match-error"),
     email: () => document.getElementById("email"),
-    EmailValidError: () => document.getElementById("email-invalid-error"),
-    EmailRequiredError: () => document.getElementById("email-required-error"),
-    password: () => document.getElementById("password")
+    emailInvalidError: () => document.getElementById("email-invalid-error"),
+    emailRequiredError: () => document.getElementById("email-required-error"), // Corrigido para "emailRequiredError"
+    passwordRequiredError: () => document.getElementById("password-required-error"),
+    passwordMinLengthError: () => document.getElementById("password-min-length-error"), // Corrigido para "password-min-length-error"
+    password: () => document.getElementById("password"),
+    registerButton : () => document.getElementById("register-button")
+};
+
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
 }
